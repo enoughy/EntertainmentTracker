5024393b66cdce66940d04431f9540d229376c2f
123 <you@example.com>
2026-07-31 02:29:58 +0700

    add dount chart element

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+"use client";
 import Image from "next/image";
 import { BaseStatCard } from "@/features/base-stat-card/base-stat-card";
+import { DountChart } from "@/features/dount-chart/dount-chart";
+import { DountChartData } from "@/types/dount-chart-stat/dount-chart-stat";
+
+const data: DountChartData = {
+  data: [
+    { name: "favorite", value: 20 },
+    { name: "dropped", value: 10 },
+    { name: "completed", value: 20 },
+    { name: "planning", value: 70 },
+    { name: "in_progress", value: 70 },
+  ],
+};
 
 export default function Home() {
   return (
@@ -10,6 +23,7 @@ export default function Home() {
         countChange={5}
         proc={-4.1}
       ></BaseStatCard>
+      <DountChart stat={data} name="Фильмы"></DountChart>
     </>
   );
 }
---
+++ features/dount-chart/dount-chart.tsx
@@ -0,0 +1,44 @@
+import { BaseCard } from "@/components/base-card/base-card";
+import { ContentStatus } from "@/types/content-status/content-status";
+import { DountChartData } from "@/types/dount-chart-stat/dount-chart-stat";
+import { Pie, PieChart, ResponsiveContainer, Sector } from "recharts";
+type DountChartProps = {
+  stat: DountChartData;
+  name: string;
+};
+
+export function DountChart({ stat, name }: DountChartProps) {
+  const STATUS_COLORS: Record<string, string> = {
+    favorite: "#FF6787",
+    in_progress: "#FFC766",
+    planning: "#9A99F4",
+    completed: "#87D68D", // "#B3FFFC",
+    dropped: "#483C46", //"#373F51",
+  };
+  return (
+    <BaseCard className="w-[207px] h-[228px] rounded-[55px] pt-8">
+      <h4 className="text-center text-[#222F3E] text-[26px] font-extrabold">
+        {name}
+      </h4>
+      <div className="h-30">
+        <ResponsiveContainer>
+          <PieChart>
+            <Pie
+              data={stat.data}
+              dataKey="value"
+              cx="50%"
+              cy="50%"
+              outerRadius={60}
+              innerRadius={30}
+              labelLine={false}
+              stroke="none"
+              shape={(props) => (
+                <Sector {...props} fill={STATUS_COLORS[props.name ?? ""]} />
+              )}
+            />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+    </BaseCard>
+  );
+}
