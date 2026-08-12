9b6bb4988f47a80834e6cbdf49fadf978f4381c5
123 <you@example.com>
2026-08-12 21:27:50 +0700

    add menu hover

--- components/status-card/status-card.tsx
+++ components/status-card/status-card.tsx
@@ -1,6 +1,7 @@
 import { ContentStatus } from "@/types/content-status/content-status";
 import { StatusCardAnim } from "./status-card-anim";
 import { STATUS_COLORS } from "@/types/content-status/status-colors";
+import { TYPE_NAME } from "@/types/content-status/content-status-name";
 
 type StatusCardProps = {
   status: ContentStatus;
@@ -8,16 +9,20 @@ type StatusCardProps = {
 export function StatusCard({ status }: StatusCardProps) {
   const cardColor = STATUS_COLORS[status];
   return (
-    <StatusCardAnim>
-      <div
-        style={{ "--card-color": cardColor } as React.CSSProperties}
-        className={`flex justify-between items-center rounded-3xl py-1 px-3 bg-[var(--card-color)]/10 hover:bg-[var(--card-color)]/20 transition-colors duration-150`}
-      >
+    <div className="inline-flex">
+      <StatusCardAnim>
         <div
-          className={`w-2 h-2 rounded-2xl bg-[var(--card-color)] mr-2`}
-        ></div>
-        <div className={`text-[14px] text-[var(--card-color)]`}>{status}</div>
-      </div>
-    </StatusCardAnim>
+          style={{ "--card-color": cardColor } as React.CSSProperties}
+          className={`inline-flex justify-between items-center rounded-3xl py-1 px-3 bg-[var(--card-color)]/10 hover:bg-[var(--card-color)]/20 transition-colors duration-150 max-w-36`}
+        >
+          <div
+            className={`w-2 h-2 rounded-2xl bg-[var(--card-color)] mr-2`}
+          ></div>
+          <div className={`text-[14px] text-[var(--card-color)]`}>
+            {TYPE_NAME[status]}
+          </div>
+        </div>
+      </StatusCardAnim>
+    </div>
   );
 }
--- features/added-recetnly/table/table-item.tsx
+++ features/added-recetnly/table/table-item.tsx
@@ -1,19 +1,27 @@
+import { StatusCard } from "@/components/status-card/status-card";
 import { Media } from "@/features/content/entity/media";
 import { TYPE_NAME } from "@/types/content-status/content-status-name";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { CiMenuKebab } from "react-icons/ci";
+import { motion } from "motion/react";
 
 type TableItemProps = {
   item: Media;
 };
 export function TableItem({ item }: TableItemProps) {
   return (
-    <div className="grid grid-cols-[7fr_6fr_6fr_6fr_1fr] text-[16px] border-b-1 border-[#CFE0FA] pb-4 mb-4 font-medium last:border-none text-text-gray">
-      <p>{item.name}</p>
-      <p>{item.genres[0] + item.genres[1]}</p>
-      <p>{item.rate}</p>
-      <p>{TYPE_NAME[item.contentStatus]}</p>
-      <BsThreeDotsVertical className="text-text-gray-2"></BsThreeDotsVertical>
+    <div className="border-b-1 border-[#CFE0FA] last:border-none mb-4 ">
+      <motion.div whileHover={{ y: -3 }}>
+        <div className="grid grid-cols-[7fr_6fr_6fr_6fr_1fr] text-[16px] pb-4 font-medium  text-text-gray ">
+          <p>{item.name}</p>
+          <p>{item.genres[0] + item.genres[1]}</p>
+          <p>{item.rate}</p>
+          <StatusCard status={item.contentStatus}></StatusCard>
+          <div className="hover:bg-slate-100 rounded-4xl transition-colors duration-100 flex items-center justify-center">
+            <BsThreeDotsVertical className="text-text-gray-2"></BsThreeDotsVertical>
+          </div>
+        </div>
+      </motion.div>
     </div>
   );
 }
